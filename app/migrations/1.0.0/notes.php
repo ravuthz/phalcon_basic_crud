abc8e732c9cf8ec8985506299f6f651f179938ed
ravuthz <ravuthz@gmail.com>
2016-11-30 21:04:04 +0700

    Create Notes Table & Migration

---
+++ app/migrations/1.0.0/notes.php
@@ -0,0 +1,93 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class NotesMigration_100
+ */
+class NotesMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('notes', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 200,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'note',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'name'
+                        ]
+                    ),
+                    new Column(
+                        'date',
+                        [
+                            'type' => Column::TYPE_DATE,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'note'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
